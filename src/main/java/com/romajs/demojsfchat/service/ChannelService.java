f941d99aafe8f1df08dfdb2240e78a1ba9143245
romajs <joseromualdoneto@gmail.com>
2016-07-05 09:21:37 -0300

    jetty: cdi + jsf

--- src/main/java/com/romajs/demojsfchat/service/ChannelService.java
+++ src/main/java/com/romajs/demojsfchat/service/ChannelService.java
@@ -15,43 +15,43 @@ import com.romajs.demojsfchat.domain.User;
 @ApplicationScoped
 public class ChannelService implements Serializable {
 
-	@PersistenceContext
-	EntityManager em;
+	@PersistenceContext // FIXME
+//	EntityManager em;
 
 	public void create(String name, User owner, User... users) {
 		Channel channel = new Channel(name, owner);
-		em.persist(channel);
+//		em.persist(channel);
 		for (User user : users) {
 			ChannelInscribed inscribed = new ChannelInscribed(channel, user);
-			em.persist(inscribed);
+//			em.persist(inscribed);
 		}
 	}
 
 	public void inscribe(Channel channel, User user) {
 		ChannelInscribed inscribed = new ChannelInscribed(channel, user);
-		em.persist(inscribed);
+//		em.persist(inscribed);
 	}
 
 	public List<Channel> listChannels() {
 		StringBuilder sql = new StringBuilder("select c from Channel c");
-		TypedQuery<Channel> query = em.createQuery(sql.toString(), Channel.class);
-		return query.getResultList();
+//		TypedQuery<Channel> query = em.createQuery(sql.toString(), Channel.class);
+		return null; //query.getResultList();
 	}
 
 	public List<User> listChannelUsers(Channel channel) {
 		StringBuilder sql = new StringBuilder("select c.u from ChannelInscribed ci where ci.channel = :c");
-		TypedQuery<User> query = em.createQuery(sql.toString(), User.class);
-		query.setParameter("c", channel);
-		return query.getResultList();
+//		TypedQuery<User> query = em.createQuery(sql.toString(), User.class);
+//		query.setParameter("c", channel);
+		return null; //query.getResultList();
 	}
 
 	public void ununscribe(Channel channel, User user) {
 		StringBuilder sql = new StringBuilder("select ci from ChannelInscribed ci");
 		sql.append("where ci.channel = :c and c.user = :u");
-		TypedQuery<ChannelInscribed> query = em.createQuery(sql.toString(), ChannelInscribed.class);
-		query.setParameter("c", channel);
-		query.setParameter("u", user);
-		ChannelInscribed inscribed = query.getSingleResult();
-		em.remove(inscribed);
+//		TypedQuery<ChannelInscribed> query = em.createQuery(sql.toString(), ChannelInscribed.class);
+//		query.setParameter("c", channel);
+//		query.setParameter("u", user);
+//		ChannelInscribed inscribed = query.getSingleResult();
+//		em.remove(inscribed);
 	}
 }
